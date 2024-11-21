966f265b1c93dee203ccbb37feb3b9742d3846c8
Angelito <ezkyuhiro24@gmail.com>
2024-11-21 21:56:43 +0800

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,91 @@
+const logotext = "ANGELITO";
+const meta = {
+    title: "Angelito Domingo",
+    description: "I’m Angelito Domingo code scientist _ Aspiring Software Developer,currently working in Philippines",
+};
+
+const introdata = {
+    title: "I’m Angelito Domingo, Aspiring Software Developer",
+    animated: {
+        first: "I love coding",
+        second: "Proficient on copying things-haha!",
+        third: "eager to learn while doing",
+    },
+    description: "I am excited to be part of your company and grow as a team and productive individual.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "",
+};
+const worktimeline = [
+    
+];
+
+const skills = [
+    {
+        name: "",
+        //value:
+    }
+];
+
+const services = [
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/photo-1508736793122-f516e3ba5569?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Promotion of a burger product consist of static menu, still in development.",
+        link: "https://vercel.com/skrfffe1s-projects/fcbportfolio/HEqnCR4RJXFHN2koS9dbUgEC8eWW",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1639843906796-a2c47fc24330?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "NFT Staking dapp, during web3 hackathon. ",
+        link: "https://vercel.com/skrfffe1s-projects/simple-dapp/fLLmE3B7VnkyLFQdbueGfyhrd1pH",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "My first nextjs blog web app",
+        link: "https://nextjs-blog-eta-cyan.vercel.app/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1692158962127-badc3b8d6a0f?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Todo List ",
+        link: "https://todo-app-omega-cyan.vercel.app/",
+    },
+    {
+        img: "",
+        description: "More projects to come ",
+        link: "",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "com.dan.atwork@gmail.com",
+    YOUR_FONE: "+639302410985",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/skrfffe1",
+    facebook: "https://www.facebook.com/skrfffe1",
+    linkedin: "https://www.linkedin.com/in/angelito-domingo-0930tac/",
+    twitter: "https://x.com/SundayAngel28",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
